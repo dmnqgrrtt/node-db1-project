282a766362a2dcf275e7ccdf48e6af9f3a8758fa
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-07-22 13:16:31 -0400

    finished all endpoints

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -22,4 +22,51 @@ router.get("/:id", (req,res) => {
     .catch(err => console.log(err));
 })
 
+router.post("/", (req,res)=>{
+    const accountInfo = req.body;
+    if(accountInfo.name){
+        if(accountInfo.budget) {
+            db("accounts")
+            .insert(accountInfo,)
+            .then(id => res.status(201).json({data: id}))
+            .catch(err => console.log(err));
+        } else {
+            res.status(404).json({message: "Missing required budget field"})
+        }
+    } else {
+        res.status(404).json({message: "Missing required name field"})
+    }
+})
+
+router.put("/:id", (req,res)=>{
+    const {id} = req.params;
+    const updates = req.body;
+    db("accounts")
+    .where("id", id)
+    .update(updates)
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({data: count})
+        } else {
+            res.status(404).json({message: "There is no account that matches that id provided"})
+        }
+    })
+    .catch(err => console.log(err));
+})
+
+router.delete("/:id", (req,res)=>{
+    const {id} = req.params;
+    db("accounts")
+    .where("id", id)
+    .del()
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({data: count})
+        } else {
+            res.status(404).json({message: "There is no account that matches that id provided"})
+        }
+    })
+    .catch(err => console.log(err));
+})
+
 module.exports = router;
